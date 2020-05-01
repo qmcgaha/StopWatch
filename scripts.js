f5b027d49190be752094fca33a40279c0660c164
Qmcgaha <qmcgaha@gmail.com>
2020-04-30 19:38:05 -0500

    Stop button works and disables start button

--- scripts.js
+++ scripts.js
@@ -4,19 +4,29 @@
 
 
 
+const secondsElement = document.getElementById("seconds-elapsed");
+let seconds = 0;
+let interval;
 
-const secondsElement = document.getElementById("seconds-elapsed")
-let seconds = 0
+function displaySeconds() {
+  secondsElement.innerText = seconds;
+}
 
 // A "Start" button to start the stopwatch running.
-document.getElementById("start").onclick = function () {
-  setInterval(() => {
+const startButton = document.getElementById("start")
+startButton.onclick = function () {
+  interval = setInterval(() => {
+    startButton.setAttribute("disabled", true);
     seconds++;
-    secondsElement.innerText = seconds
+    displaySeconds();
   }, 1000);
 }
-//   A "Stop" button to pause / stop it.
 
+//   A "Stop" button to pause / stop it.
+document.getElementById("stop").onclick = function () {
+  clearInterval(interval);
+  startButton.removeAttribute("disabled");
+}
 //     A "Reset" button to reset the time back to 0.
 
 // The time display to show the number of seconds elapsed, rather than the actual time.
